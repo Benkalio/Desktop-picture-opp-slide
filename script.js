1c3513a7f4f34fa989e7ad69fdf62db22e6ca8cb
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-10-01 05:31:23 +0100

    Implemented Javascript for the behavior Picture slides evenly

---
+++ script.js
@@ -0,0 +1,40 @@
+const containerSlider = document.querySelector(".container-slider"),
+  rightSlide = document.querySelector(".slide-right"),
+  leftSlide = document.querySelector(".slide-left"),
+  upButton = document.querySelector(".up-button"),
+  downButton = document.querySelector(".down-button"),
+  //TARGETING ALL THE DIVS WITH BACKGROUND IMAGES LENGTH
+  slidesLength = rightSlide.querySelectorAll("div").length;
+
+let activeSlideIndex = 0;
+
+leftSlide.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+if (upButton) {
+  upButton.addEventListener("click", () => changeSlide("up"));
+}
+if (downButton) {
+  downButton.addEventListener("click", () => changeSlide("down"));
+}
+const changeSlide = (direction) => {
+  const sliderHeight = containerSlider.clientHeight;
+
+  //DIRECTION THAT THE ARROW IS CLICKED TO MOVE
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    } else if (direction === "down") {
+      activeSlideIndex--;
+      if (activeSlideIndex < 0) {
+        activeSlideIndex = slidesLength - 1;
+      }
+    }
+  }
+  rightSlide.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+  leftSlide.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
